75a20cacb516c9eab721c38e5945b9cc0e372431
balthair94 <archerbaltazar@gmail.com>
2017-02-01 22:48:22 -0600

    Splash activity added and workig

---
+++ app/src/main/java/baltamon/mx/localstoragepractice/activities/SplashActivity.java
@@ -0,0 +1,32 @@
+package baltamon.mx.localstoragepractice.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import baltamon.mx.localstoragepractice.MainActivity;
+import baltamon.mx.localstoragepractice.R;
+
+/**
+ * Created by Baltazar Rodriguez on 01/02/2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 3000);
+
+    }
+}
